0e7c50348108c0589119f58c14dc9f192818534d
Chaitanya Hazarika <chaitanyahazarika@Ananyas-MacBook-Air.local>
2024-10-07 22:23:41 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+
+    return (
+        <footer>
+            <p>&copy;{new Date().getFullYear()} My first react website</p>
+        </footer>
+    );
+
+}
+
+export default Footer
